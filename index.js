22043f6888bcb8ff8839a4bdd462a4d914a3f3b1
Larry Otieno <larryotieno51@gmail.com>
2025-03-25 13:06:23 +0300

    Fetchs and displays events dynamically

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const eventLists = document.getElementById("event-list");
+    const API_URL = "http://localhost:3000/events";
   
+    function fetchEvents() {
+      fetch(API_URL)
+        .then((response) => response.json())
+        .then((events) => displayEventList(events))
+        .catch((error) =>
+          console.error("Error while fetching the events", error)
+        );
+    }
+  
+    function displayEventList(events) {
+      eventLists.innerHTML = ""; // clears the elements
+      events.forEach((event) => {
+        const eventCard = document.createElement("div");
+        eventCard.classList.add("event-card");
+        eventCard.innerHTML = `
+              <h3>${event.name}</h3>
+              <p>${event.date} - ${event.location}</p>
+          `;
+          eventLists.appendChild(eventCard);
+      });
+    }
+  
+    fetchEvents();
 });
